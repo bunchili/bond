6b6b807326e82b7093e249d344f66ba69f9c295d
bunchili <bunchili@hanmail.net>
2016-03-06 11:53:31 +0000

    posts_category 액션과 뷰 만들기, application 레이아웃 수정하기

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,6 +4,17 @@ class FoodsController < ApplicationController
   end
 
   def posts_category
+    case params[:category]
+    when "PreviousClosingYield"
+      @category = "민평표"
+    when "YieldPrice"
+      @category = "단가표"
+    when "KTBissue"
+      @category = "국고발행계획"
+    else
+      @category = "채권뉴스"
+    end
+    @posts = Post.where(category: @category)
   end
 
   def show
