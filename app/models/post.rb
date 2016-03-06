805f2d4aef43aafd706397bed923cc22a053363a
bunchili <bunchili@hanmail.net>
2016-03-06 18:27:05 +0000

    carrierwave gem 사용 준비하기

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   has_many  :comments
   validates :category, :inclusion => { :in => ["민평표","단가표","국고발행계획","채권뉴스"], :message => "민평표, 단가표, 국고발행계획, 채권뉴스 중 하나를 선택하셔야 합니다." }
   validates :title, :presence => { :message => "제목을 반드시 입력하셔야 합니다." }
+  mount_uploader :image, ImageUploader
 end
