bedbca680a1b1eade41b4afc34bfc5ba82ec2632
bunchili <bunchili@hanmail.net>
2016-03-06 09:54:37 +0000

    마이그레이션 파일과 모델 파일 작성하고 User, Post, Comment 테이블 만들기

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ActiveRecord::Base
+  belongs_to  :user
+  has_many  :comments
+  validates :category, :inclusion => { :in => ["민평표","단가표","국고발행계획","채권뉴스"], :message => "민평표, 단가표, 국고발행계획, 채권뉴스 중 하나를 선택하셔야 합니다." }
+  validates :title, :presence => { :message => "제목을 반드시 입력하셔야 합니다." }
+end
