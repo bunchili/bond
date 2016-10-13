0df29c345529e8915cb5095ebd7fbeca91955135
bunchili <bunchili@hanmail.net>
2016-10-13 13:14:25 +0000

    완성본

--- app/controllers/trading_controller.rb
+++ app/controllers/trading_controller.rb
@@ -1,10 +1,10 @@
 class TradingController < ApplicationController
   def bidask
-    @trading_all = Trading.take(9)
+    @trading_all = Trading.take(10)
   end
   
   def getdata
-	@trading_all = Trading.take(9)
+	@trading_all = Trading.take(10)
 	respond_to do |format|
 		format.html { render :partial => "bidask", :locals => { :trading_all => @trading_all } }
 	end
